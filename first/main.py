ed17eb1d23ff37581595ac54c058410b2fa87471
Sergey3291 <sergey3291@gmail.com>
2023-08-13 10:47:10 +0300

    first commit

---
+++ first/main.py
@@ -0,0 +1,15 @@
+# main.py
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+#@app.get("/")
+#async def root():
+#    return {"message": "Hello World"}
+
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
